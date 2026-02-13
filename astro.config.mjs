518dbf1beb7a3a6b77044d7658ee62611a2db06a
Lakshman <lakshman@moiiai.com>
2026-02-13 14:27:25 +0100

    Refactor base URL handling in navigation and blog links for consistency

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lab176344.github.io',
-  base: '/lb.github.io',
+  base: '/writeup',
   integrations: [tailwind()],
 });
