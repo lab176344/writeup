157b3317767a2b53f3bdf6d61f077e8d9dc3d81e
Lakshman <lakshman@moiiai.com>
2026-02-13 14:25:55 +0100

    Update base path in configuration to match deployment structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lab176344.github.io',
-  base: '/',
+  base: '/lb.github.io',
   integrations: [tailwind()],
 });
